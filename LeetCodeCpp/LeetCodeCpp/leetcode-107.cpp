a244c8a353e1f7f4f89ed61bce70bfdf7e24cdd3
Brian <1786104060@qq.com>
2019-10-29 12:41:44 +0800

    编写leetcode-107

---
+++ LeetCodeCpp/LeetCodeCpp/leetcode-107.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	vector<vector<int>> levelOrderBottom(TreeNode* root) {
+		vector<vector<int>> result;
+		if (root == NULL)
+			return result;
+		//使用BFS，一个队列记录结点，一个队列记录对应的几点是第几层
+		stack<vector<int>> resStack;
+		queue<TreeNode*> qNode;
+		queue<int> qLv;
+		qNode.push(root); qLv.push(0);
+		vector<int> tempRes;
+		int cur = 0;
+		while (!qNode.empty()) {
+			//逻辑保证每次取出的tempNode不为NULL
+			TreeNode* tempNode = qNode.front();
+			int tempLv = qLv.front();
+			qNode.pop(); qLv.pop();
+			if (tempLv != cur) {
+				resStack.push(tempRes);
+				tempRes.clear();
+				tempRes.push_back(tempNode->val);
+				cur = tempLv;
+			}
+			else {
+				tempRes.push_back(tempNode->val);
+			}
+			//将tempNode不为空的左右子节点入队
+			if (tempNode->left) {
+				qNode.push(tempNode->left);
+				qLv.push(cur + 1);
+			}
+			if (tempNode->right) {
+				qNode.push(tempNode->right);
+				qLv.push(cur + 1);
+			}
+		}
+		//将最后一层加入result
+		result.push_back(tempRes);
+		//将栈里的vector加入result
+		while (!resStack.empty()) {
+			result.push_back(resStack.top());
+			resStack.pop();
+		}
+		return result;
+	}
+};
