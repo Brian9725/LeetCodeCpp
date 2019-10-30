cd659d55a0ea930f99b3b20373d8e40b1ce81aff
Brian <1786104060@qq.com>
2019-10-31 00:19:51 +0800

    编写leetcode-129

---
+++ LeetCodeCpp/LeetCodeCpp/leetcode-129.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+	void dfs(int &result, int &tempRes, TreeNode* node) {
+		if (node->left == NULL && node->right == NULL) {
+			tempRes = tempRes * 10 + node->val;
+			result += tempRes;
+			tempRes /= 10;
+			return;
+		}
+		tempRes = tempRes * 10 + node->val;
+		if (node->left)
+			dfs(result, tempRes, node->left);
+		if (node->right)
+			dfs(result, tempRes, node->right);
+		tempRes /= 10;
+	}
+
+	int sumNumbers(TreeNode* root) {
+		if (root == NULL)
+			return 0;
+		int result = 0, tempRes = 0;
+		dfs(result, tempRes, root);
+		return result;
+	}
+};
