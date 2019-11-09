7c99af52144a181e7ac0787e26dd2933093d8112
Brian <1786104060@qq.com>
2019-11-09 23:06:34 +0800

    编写leetcode-81

--- LeetCodeCpp/LeetCodeCpp/leetcode-81.cpp
+++ LeetCodeCpp/LeetCodeCpp/leetcode-81.cpp
@@ -4,10 +4,27 @@ using namespace std;
 
 class Solution {
 public:
-	int findMin(vector<int>& nums) {
+	//在nums[low..high]上二分查找target
+	int binSearch(vector<int>& nums, int low, int high, int target) {
+		int result = -1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (target == nums[mid]) {
+				result = mid; break;
+			}
+			else if (target >= nums[mid])
+				low = mid + 1;
+			else
+				high = mid - 1;
+		}
+		return result;
+	}
+
+	//类似leetcode-154
+	bool search(vector<int>& nums, int target) {
 		int len = nums.size();
 		if (len == 0)
-			return -1;
+			return false;
 		//minIdx为最小元素的下标
 		int low = 0, high = len - 1, minIdx = 0;
 		while (low <= high) {
@@ -29,6 +46,19 @@ public:
 				}
 			}
 		}
-		return nums[minIdx];
+		int result = -1, mid = 0;
+		if (minIdx == 0)
+			result = binSearch(nums, 0, len - 1, target);
+		//在nums[0..minIdx-1]上二分查找target
+		else if (target >= nums[0])
+			result = binSearch(nums, 0, minIdx - 1, target);
+		//在nums[minIdx..len-1]上二分查找target
+		else if (target <= nums[len - 1])
+			result = binSearch(nums, minIdx, len - 1, target);
+		else
+			result = -1;
+		if (result == -1)
+			return false;
+		return true;
 	}
 };
