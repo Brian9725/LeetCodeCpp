3d8084486b9e80b78c03b03ec9426918328513d0
Brian <1786104060@qq.com>
2019-11-16 17:04:01 +0800

    更新leetcode-1

--- LeetCodeCpp/LeetCodeCpp/leetcode-1.cpp
+++ LeetCodeCpp/LeetCodeCpp/leetcode-1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
+#include<map>
 #include<vector>
 using namespace std;
 
 class Solution {
 public:
+	//hash
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> result;
+		int len = nums.size();
+		map<int, int> numMap;
+		for (int i = 0; i < len; i++) {
+			if (numMap.find(target - nums[i]) != numMap.end()) {
+				result.push_back(numMap[target - nums[i]]);
+				result.push_back(i);
+				break;
+			}
+			numMap[nums[i]] = i;
+		}
+		return result;
+	}
+
+	/*
 	vector<int> twoSum(vector<int>& nums, int target) {
 		vector<int> result;
 		int len = nums.size();
@@ -19,4 +37,5 @@ public:
 		}
 		return result;
 	}
+	*/
 };
