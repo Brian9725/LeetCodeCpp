c4680d32f92b0b8ba17ca22ca0e471d79421180f
Brian <1786104060@qq.com>
2019-11-20 20:36:23 +0800

    编写leetcode-219

---
+++ LeetCodeCpp/LeetCodeCpp/leetcode-219.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<algorithm>
+#include<map>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	bool containsNearbyDuplicate(vector<int>& nums, int k) {
+		map<int, int> numMap;
+		for (int i = 0; i < nums.size(); i++) {
+			if (numMap.find(nums[i]) == numMap.end() || abs(i - numMap[nums[i]]) > k)
+				numMap[nums[i]] = i;
+			else
+				return true;
+		}
+		return false;
+	}
+};
