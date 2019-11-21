839eeb82aaa096a5baf0c10045ce1365a39fc611
Brian <1786104060@qq.com>
2019-11-21 23:23:49 +0800

    更新leetcode-274

--- LeetCodeCpp/LeetCodeCpp/leetcode-274.cpp
+++ LeetCodeCpp/LeetCodeCpp/leetcode-274.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 class Solution {
 public:
+	int hIndex(vector<int>& citations) {
+		vector<int> num(citations.size() + 1, 0);
+		for (int i = 0; i < citations.size(); i++) {
+			if (citations[i] > citations.size())
+				citations[i] = citations.size();
+			num[citations[i]]++;
+		}
+		int result = 0;
+		for (int i = citations.size(); i >= 0; i--) {
+			if (num[i] + result < i)
+				result += num[i];
+			else
+				return i;
+		}
+		return 0;
+	}
+
+	/*ÅÅÐò
 	struct cmp {
 		bool operator()(int&x, int&y) {
 			return y < x;
@@ -22,4 +40,5 @@ public:
 		}
 		return result;
 	}
+	*/
 };
