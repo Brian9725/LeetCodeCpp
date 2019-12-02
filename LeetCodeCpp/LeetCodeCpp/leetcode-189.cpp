3535d8f622be1367d48a75e71723c814731a8f7f
Brian <1786104060@qq.com>
2019-12-02 22:48:07 +0800

    编写leetcode-189

---
+++ LeetCodeCpp/LeetCodeCpp/leetcode-189.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	void rotate(vector<int>& nums, int k) {
+		if (nums.size() == 0 || k == 0 || k%nums.size() == 0) return;
+		int len = nums.size(), n = len, idx = 0, cur = 0, temp = nums[idx];
+		k %= n;
+		while (n--) {
+			int next = (cur + len - k) % len;
+			if (next == idx) {
+				nums[cur] = temp; 
+				idx++; cur = idx; temp = nums[idx];
+				continue;
+			}
+			nums[cur] = nums[(cur + len - k) % len];
+			cur = (cur + len - k) % len;
+		}
+	}
+};
+
+int main() {
+	vector<int> v = { 1,2,3,4,5,6 };
+	Solution().rotate(v, 2);
+	return 0;
+}
