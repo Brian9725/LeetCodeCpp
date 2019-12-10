a2b7c12d97fea5fb3a240a0ac6f9d022ac6014c5
Brian <1786104060@qq.com>
2019-12-10 11:05:15 +0800

    编写leetcode-215

---
+++ LeetCodeCpp/LeetCodeCpp/leetcode-215.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<functional>
+#include<queue>
+using namespace std;
+
+class Solution {
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+		priority_queue<int, vector<int>, greater<int>> pq;
+		for (int i = 0; i < (int)nums.size(); ++i) {
+			pq.push(nums[i]);
+			if (pq.size() > k) pq.pop();
+		}
+		return pq.top();
+	}
+};
